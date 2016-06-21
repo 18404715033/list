3246b00f649f86c5fab33783152edae9e031cef8
18404715033 <965630447@qq.com>
2016-06-21 17:26:39 +0800

    Initial Commit

---
+++ list/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  list
+//
+//  Created by 李伯阳  Lok’Tar Ogar on 16/6/21.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
