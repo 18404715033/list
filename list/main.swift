e4ff0eaee1a4b49c91f0b0c02dfb45369640b530
18404715033 <965630447@qq.com>
2016-06-21 18:06:12 +0800

    list
     用冒泡排序实现了把一组数从大到小或从小到大进行排序。

--- list/main.swift
+++ list/main.swift
@@ -1,12 +1,26 @@
-//
-//  main.swift
-//  list
-//
-//  Created by 李伯阳  Lok’Tar Ogar on 16/6/21.
-//  Copyright © 2016年 Apple. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+import Cocoa
+var List = [1,2,3,445,666,65]
+print ("排序前的值：")
+for item in List
+{
+    var ii=item
+    print(ii)
+}
+for i in 0..<List.count - 1
+{
+    for j in 0..<List.count - 1 - i
+    {
+        if List[j] > List[j+1]
+        {
+            var temp = List[j+1]
+            List[j+1]=List[j]
+            List[j]=temp
+        }
+    }
+}
+print ("排序后的值：")
+for item in List
+{
+    var ii=item
+    print (ii)
+}
